9c5c71a591c4fc08e43d474a441b56417d064cf8
MartinM1409 <martinmeria1409@gmail.com>
2025-05-15 12:02:40 +0300

    Proiect exportat din Replit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/ExdeStat_2025/", // adaugă această linie
   plugins: [
     react(),
     runtimeErrorOverlay(),
